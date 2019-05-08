2d343b0a0b3c9799fca4f94646114e9badac0b42
Dave Kanter <moatgoat@hotmail.com>
2019-05-08 14:58:28 -0700

    Added two pages and changed app.js

---
+++ routing/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+    )
+  }
+}
+
+export default App;
---
+++ routing/src/users.js
@@ -0,0 +1,9 @@
+import React ferom 'react'
+
+class Users extends React.Component {
+	render() {
+		return <h1>Users</h1>
+	}
+}
+
+export default Users
